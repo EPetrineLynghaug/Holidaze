4ac59cd30e98552fdba45f07b4fbcbde1157ed18
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-04-14 16:18:41 +0200

    api working on AllVenues page

--- src/constans/Layout.jsx
+++ src/constans/Layout.jsx
@@ -7,7 +7,8 @@ export default function Layout() {
       <header>
         {/* Enkel navigasjon i headeren */}
         <nav>
-          <Link to="/">Hjem</Link> <Link to="/login">Login</Link>{" "}
+          <Link to="/">Hjem</Link>{" "}
+          <Link to="/login">Login</Link>{" "}
           <Link to="/register">Register</Link>{" "}
           <Link to="/venues">All Venues</Link>{" "}
           <Link to="/profile">Profile</Link>
