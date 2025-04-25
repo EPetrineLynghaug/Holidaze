a5357cce235b871e7b1342f961417b031ead45a0
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-04-25 10:26:01 +0200

    working on mini animasjon on register

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import VenueDetail from './pages/VenueDetail';
 
 
 import Login from './pages/Login';
-import Register from './components/registerAndLogin/Register';
+import Register from './pages/Register'
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import './styles/icons.css';
 import "./index.css";
 import { BrowserRouter as Router } from "react-router"; 
 import { UserProvider } from "./components/context/UserContext"; 
