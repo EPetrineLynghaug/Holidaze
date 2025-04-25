69a7839963aa6a49f6c6302eecdf3035b1562483
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-04-25 09:26:56 +0200

    login and mobilemenu in working

--- src/components/constans/Layout.jsx
+++ src/components/constans/Layout.jsx
@@ -1,16 +1,17 @@
-import React, { useContext } from "react";
-import { Outlet, NavLink } from "react-router";
-
+import React from "react";
+import { Outlet, useLocation } from "react-router";
 import MainNavigation from "../navigation/MainMobileMenu";
 
 export default function Layout() {
+  const location = useLocation();
+  const hideNavOn = ["/login", "/register"];
+  const shouldHideNav = hideNavOn.includes(location.pathname);
+
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Henter inn hele header/nav med logout-logikk */}
-      <MainNavigation />
+      {!shouldHideNav && <MainNavigation />}
 
-      {/* Hovedinnhold */}
-      <main className="flex-1 p-4 max-w-screen-lg mx-auto">
+      <main className="flex-1 px-4 max-w-screen-lg mx-auto">
         <Outlet />
       </main>
     </div>
